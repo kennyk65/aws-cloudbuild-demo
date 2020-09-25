e05b50d41af2dc9ffe7ac7d0a904e2683e6a9c75
Ken Krueger <kenkrueger65@gmail.com>
2020-09-25 17:28:05 -0400

    update to spring boot / jdk 11

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,18 +1,16 @@
 package com.example;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootConfiguration()
 @WebAppConfiguration
 public class ApplicationTests {
 
-	// Really need to add a meaningful test here.
 	@Test
 	//@Ignore
 	public void contextLoads() {
